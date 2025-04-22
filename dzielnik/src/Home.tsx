bd5c63b62882977d4032534497c51027205851b5
Zofia Lorenc <looorencz@gmail.com>
2025-04-22 14:55:48 +0200

    home page

---
+++ dzielnik/src/Home.tsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="home-image-wrapper">
+            <img src="/dzielnik_logo.png" alt="dzielnik logo" className="home-image"/>
+        </div>
+    );
+}
