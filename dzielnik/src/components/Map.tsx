74664131aa8e029c4837ef57bb36fa9f6f283320
Zofia Lorenc <looorencz@gmail.com>
2025-04-23 09:13:33 +0200

    tiny changes

--- dzielnik/src/components/Map.tsx
+++ dzielnik/src/components/Map.tsx
@@ -1,28 +1,37 @@
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
-import { useEffect, useState } from "react";
-import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { LatLngExpression } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
+  const poznanCenter: LatLngExpression = [52.4064, 16.9252];
   const [geoData, setGeoData] = useState<any>(null);
 
   useEffect(() => {
-    fetch("/dzielnice-granice.geojson")
-      .then((res) => res.json())
-      .then((data) => setGeoData(data));
+    fetch('/poznan-dzielnice.geojson')
+      .then(res => res.json())
+      .then(data => setGeoData(data));
   }, []);
 
   return (
-    <MapContainer
-      center={[52.4064, 16.9252]}
-      zoom={12}
-      style={{ height: "100vh", width: "100%" }}
-    >
+    <MapContainer center={poznanCenter} zoom={12} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
-        attribution="&copy; OpenStreetMap"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; OpenStreetMap contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
 
-      {geoData && <GeoJSON data={geoData} />}
+      {geoData && (
+        <GeoJSON
+          data={geoData}
+          style={() => ({
+            color: '#7FFF00',         // jasnozielona linia - granica?
+            weight: 6,
+            opacity: 1,
+            fillColor: '#ffffff00',   // brak koloru wypełnienia
+            fillOpacity: 0.1,
+          })}
+        />
+      )}
     </MapContainer>
   );
 }
